466f4d446974daed20573e940d344b2cd77eb0dd
Mudashiru <mudashiru60@gmail.com>
2025-05-15 12:06:34 +0100

    Initial commit with Spring Boot project

---
+++ hes/src/main/java/com/memmcol/hes/HesApplication.java
@@ -0,0 +1,13 @@
+package com.memmcol.hes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HesApplication.class, args);
+    }
+
+}
