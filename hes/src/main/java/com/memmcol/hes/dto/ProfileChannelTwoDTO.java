980e2fed2f1be4098740e7d6b017fbf87ea66277
Oluwin <holluwin@gmail.com>
2026-03-31 14:29:11 +0100

    refactored codebase for channel 2 read

--- hes/src/main/java/com/memmcol/hes/dto/ProfileChannelTwoDTO.java
+++ hes/src/main/java/com/memmcol/hes/dto/ProfileChannelTwoDTO.java
@@ -15,7 +15,17 @@ public class ProfileChannelTwoDTO {
     private String meterSerial;
     private String modelNumber;
     private LocalDateTime entryTimestamp;
-    private Double totalImportActiveEnergy;
-    private Double totalExportActiveEnergy;
+    private Integer meterHealthIndicator;
+    private Double activeEnergyImport;
+    private Double activeEnergyImportRate1;
+    private Double activeEnergyImportRate2;
+    private Double activeEnergyImportRate3;
+    private Double activeEnergyImportRate4;
+    private Double activeEnergyCombinedTotal;
+    private Double activeEnergyExport;
+    private Double reactiveEnergyImport;
+    private Double reactiveEnergyExport;
+    private Double apparentEnergyImport;
+    private Double apparentEnergyExport;
     private LocalDateTime receivedAt;
 }
