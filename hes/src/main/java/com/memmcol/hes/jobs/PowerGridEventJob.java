9012a85e5fd6bb3abf37d5a3fae445488ee90fac
H.Soliu <abeeb.ilias@gmail.com>
2026-04-01 18:46:48 +0100

    Job schedule fix

---
+++ hes/src/main/java/com/memmcol/hes/jobs/PowerGridEventJob.java
@@ -0,0 +1,33 @@
+package com.memmcol.hes.jobs;
+
+import com.memmcol.hes.jobs.services.ProfileExecutionService;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@DisallowConcurrentExecution
+@Component
+public class PowerGridEventJob extends QuartzJobBean {
+
+    @Autowired
+    private ProfileExecutionService profileExecutionService;
+
+    public PowerGridEventJob(ProfileExecutionService profileExecutionService) {
+        this.profileExecutionService = profileExecutionService;
+    }
+
+    //Required by Quartz
+    public PowerGridEventJob() {}
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        String obisCode = context.getMergedJobDataMap().getString("obisCodes");
+        log.info("✅ Executing DailyBillingJob at {}, obis={}", context.getFireTime(), obisCode);
+        profileExecutionService.readEventsForAll(obisCode); // <-- dynamic OBIS now
+    }
+}
---
+++ hes/src/main/java/com/memmcol/hes/jobs/StandardEventJob.java
@@ -0,0 +1,34 @@
+package com.memmcol.hes.jobs;
+
+import com.memmcol.hes.jobs.services.ProfileExecutionService;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@DisallowConcurrentExecution
+@Component
+public class StandardEventJob extends QuartzJobBean {
+
+    @Autowired
+    private ProfileExecutionService profileExecutionService;
+
+    public StandardEventJob(ProfileExecutionService profileExecutionService) {
+        this.profileExecutionService = profileExecutionService;
+    }
+
+    //Required by Quartz
+    public StandardEventJob() {}
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        String obisCode = context.getMergedJobDataMap().getString("obisCodes");
+        log.info("✅ Executing DailyBillingJob at {}, obis={}", context.getFireTime(), obisCode);
+        profileExecutionService.readEventsForAll(obisCode); // <-- dynamic OBIS now
+    }
+}
