dc4156c7756b20dfafdcd4ba42afb9f39a457b54
H.Soliu <abeeb.ilias@gmail.com>
2026-08-26 13:03:02 +0100

    prometheus route permit

--- hes/src/main/java/com/memmcol/hes/config/SecurityConfig.java
+++ hes/src/main/java/com/memmcol/hes/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/api/realtime/stream",
                                 "/api/realtime/meter-status/stream",
-                                "/api/actuator/prometheus"
+                                "/actuator/prometheus"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
